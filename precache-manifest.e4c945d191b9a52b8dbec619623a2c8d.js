e47961aa7bfa1a2e818721fcb1cd76d7a71be2f2
andreaHusseini <andreaHusseiniGit@github.com>
2020-10-14 02:15:02 +0100

    Updates

---
+++ precache-manifest.e4c945d191b9a52b8dbec619623a2c8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf897980dde3330bbb95b9adfe17a515",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "1a90a1f1e785ff2cb8b9",
+    "url": "/website/static/css/main.f519b658.chunk.css"
+  },
+  {
+    "revision": "7c56ff277638ed479f33",
+    "url": "/website/static/js/2.d1dea555.chunk.js"
+  },
+  {
+    "revision": "8d976e4dd7d514b4f199d3746e58ca30",
+    "url": "/website/static/js/2.d1dea555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a90a1f1e785ff2cb8b9",
+    "url": "/website/static/js/main.83f65556.chunk.js"
+  },
+  {
+    "revision": "abc8717cb9135f0ecec8",
+    "url": "/website/static/js/runtime-main.5fba4ef4.js"
+  }
+]);
